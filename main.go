4b8b2d934f6f5ef6f35784d9b16c8952747d532a
Anthony Kugel <anthonykugel@gmail.com>
2024-04-12 14:47:18 -0500

    setup events api endpoint

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Event struct {
+	ID       int
+	Name     string
+	Location string
+	Date     time.Time
+	Invitees int
+}
+
+func NewEvent(id int, name string, location string, date time.Time, invitees int) *Event {
+	return &Event{
+		ID:       id,
+		Name:     name,
+		Location: location,
+		Date:     date,
+		Invitees: invitees,
+	}
+
+}
 
 func main() {
+
 	fmt.Println("go event app")
+	r := gin.Default()
+	r.GET("/events", getEvents)
+	r.Run(":8080")
 }
+
+func getEvents(context *gin.Context) {
+	event1 := NewEvent(1, "Wedding", "Downtown Abbey", time.Now(), 400)
+	event2 := NewEvent(2, "Funeral", "St Peters Cathedral", time.Now(), 25)
+	event3 := NewEvent(3, "Deposition", "Downtown Courthouse", time.Now(), 400)
+	events := []Event{*event1, *event2, *event3}
+
+	context.JSON(http.StatusOK, events)
+}
+
+//gin.H{}
